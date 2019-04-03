dd60a1880b28e79d968b37369607e48df0cb5968
Cesar Romagnolo <cesar.romagnolo@dextra-sw.com>
2019-04-03 13:14:24 -0300

    Running Project

---
+++ OhmBass.h
@@ -0,0 +1,48 @@
+#ifndef __OHMBASS__
+#define __OHMBASS__
+
+#include "IPlug_include_in_plug_hdr.h"
+#pragma clang diagnostic pop
+
+#include "VoiceManager.h"
+#include "MIDIReceiver.h"
+#include <functional>
+
+
+class OhmBass : public IPlug
+{
+public:
+	OhmBass(IPlugInstanceInfo instanceInfo);
+	~OhmBass();
+
+	void Reset();
+	void OnParamChange(int paramIdx);
+	void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+	// to receive MIDI messages:
+	void ProcessMidiMsg(IMidiMsg* pMsg);
+
+	// Needed for the GUI keyboard:
+	// Should return non-zero if one or more keys are playing.
+	inline int GetNumKeys() const { return mMIDIReceiver.getNumKeys(); };
+	// Should return true if the specified key is playing.
+	inline bool GetKeyStatus(int key) const { return mMIDIReceiver.getKeyStatus(key); };
+	static const int virtualKeyboardMinimumNoteNumber = 23;
+	int lastVirtualKeyboardNoteNumber;
+
+private:
+	double mFrequency;
+	void CreatePresets();
+	MIDIReceiver mMIDIReceiver;
+	IControl* mVirtualKeyboard;
+	void processVirtualKeyboard();
+	VoiceManager voiceManager;
+
+	EnvelopeGenerator mFilterEnvelopeGenerator;
+
+	Oscillator mLFO;
+
+	void CreateParams();
+	void CreateGraphics();
+};
+
+#endif
---
+++ Oscillator.h
@@ -0,0 +1,52 @@
+//
+//  Oscillator.h
+//  Synthesis
+//
+//  Created by Martin on 08.04.14.
+//
+//
+
+#ifndef __Spaciala__Oscillator__
+#define __Spaciala__Oscillator__
+
+#include <math.h>
+
+class Oscillator {
+public:
+	enum OscillatorMode {
+		OSCILLATOR_MODE_SINE = 0,
+		OSCILLATOR_MODE_SAW,
+		OSCILLATOR_MODE_SQUARE,
+		OSCILLATOR_MODE_TRIANGLE,
+		kNumOscillatorModes
+	};
+	void setMode(OscillatorMode mode);
+	void setFrequency(double frequency);
+	void setSampleRate(double sampleRate);
+	void generate(double* buffer, int nFrames);
+	void setPitchMod(double amount);
+	virtual double nextSample();
+	Oscillator() :
+		mOscillatorMode(OSCILLATOR_MODE_SINE),
+		mPI(2 * acos(0.0)),
+		twoPI(2 * mPI),
+		mPitchMod(0.0),
+		mFrequency(440.0),
+		mPhase(0.0){
+			updateIncrement();
+	};
+	void reset() { mPhase = 0.0; }
+protected:
+	double naiveWaveformForMode(OscillatorMode mode);
+	OscillatorMode mOscillatorMode;
+	const double mPI;
+	const double twoPI;
+	double mFrequency;
+	double mPhase;
+	static double mSampleRate;
+	double mPhaseIncrement;
+	void updateIncrement();
+	double mPitchMod;
+};
+
+#endif /* defined(__Spaciala__Oscillator__) */
